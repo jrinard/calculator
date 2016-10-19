382f9c75eabad7d9a958be9dc63d589052af9728
Joshua Rinard and Luke Keysboe <luke@>
2016-10-19 13:27:28 -0700

    Add Sub, Mul,Div to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ var add = function(n1, n2) {
 // Subtraction
 
 var subtract = function(n1, n2) {
-  return n1 + n2;
+  return n1 - n2;
 };
 
 // Multiplication
@@ -35,4 +35,30 @@ $(document).ready(function() {
     var result = (add(num1, num2));
     $("#output").text(result);// calling -  takes result and returns text and places in output in html
   });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var num1 = parseInt($("#subtract1").val());
+    var num2 = parseInt($("#subtract2").val());
+    var result = (subtract(num1, num2));
+    $("#output").text(result);// calling -  takes result and returns text and places in output in html
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var num1 = parseInt($("#multiply1").val());
+    var num2 = parseInt($("#multiply2").val());
+    var result = (multiply(num1, num2));
+    $("#output").text(result);// calling -  takes result and returns text and places in output in html
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var num1 = parseInt($("#divide1").val());
+    var num2 = parseInt($("#divide2").val());
+    var result = (divide(num1, num2));
+    $("#output").text(result);// calling -  takes result and returns text and places in output in html
+  });
+
+
 });
